27cd8f5f2c040e3b0acb68898e4c1b59c5f067cb
Razalp <razalp0012300@gmail.com>
2025-09-27 17:32:23 +0530

    feat: add Button and Card components with variants and utility classes
    - Implemented a Button component with customizable variants and sizes using class-variance-authority.
    - Created a Card component along with its subcomponents (CardHeader, CardTitle, CardDescription, CardAction, CardContent, CardFooter) for structured content display.
    - Utilized utility functions for class name management to ensure consistent styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { FloatingDockDemo } from './components/Header/FloatingDock'
 import LandingPage from './components/LandingPage/LandingPage'
+import PixelBlast from './components/PixelBlast/PixelBlast'
 
 function App() {
   return (
     <div className="bg-black">
+    
       <FloatingDockDemo />
       <LandingPage />
+    
     </div>
   )
 }
